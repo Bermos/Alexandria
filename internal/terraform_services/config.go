afcd94cae7421a11045799d38f02ebabf761bb5c
Bermos <accounts@machivenyika.ch>
2023-03-04 14:23:25 +0100

    Add basic terraform registry scaffold

---
+++ internal/terraform_services/config.go
@@ -0,0 +1,32 @@
+package terraform_services
+
+import "fmt"
+
+var (
+	enabledServices = map[string]string{}
+)
+
+func init() {
+	EnableService("modules.v1", "/modules")
+}
+
+func GetEnabledServices() map[string]string {
+	return enabledServices
+}
+
+func EnableService(name, endpoint string) error {
+	enabledServices[name] = endpoint
+
+	return nil
+}
+
+func DisableService(name string) error {
+	_, ok := enabledServices[name]
+	if ok {
+		delete(enabledServices, name)
+	} else {
+		return fmt.Errorf("could not remove key %s, it does not exist in the map", name)
+	}
+
+	return nil
+}
